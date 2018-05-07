2d345e43790ba7fb0c9ce75d29eaa1a94d57d067
Nelson Diaz <abnerdc86@gmail.com>
2018-05-07 16:51:53 -0400

    creating dinamic DB setup for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 const mysql = require('mysql');
-// Develoment Database Configuration
-// const config = {
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "jesus1st",
-//   database: "burger_db"
-// }
-// Production Database Configuration
-const config = {
-  host: "	vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "olly9azms36yofzh",
-  password: "s4h5v2vi74j5qzx6",
-  database: "o51nyftn4vs2ahtf"
+
+var config;
+if(process.env.JAWSDB_URL){
+  // Production Database Configuration
+  config = process.env.JAWSDB_URL;
+} else{
+  // Develoment Database Configuration
+  const config = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "jesus1st",
+    database: "burger_db"
+  }
 }
 
 var conn = mysql.createConnection(config);
