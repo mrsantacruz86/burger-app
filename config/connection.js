670ed46b292057f247b0825a2d9816651d8cca8a
Nelson Diaz <abnerdc86@gmail.com>
2018-05-07 18:37:07 -0400

    fixing responsiveness issues

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if(process.env.JAWSDB_URL){
   config = process.env.JAWSDB_URL;
 } else{
   // Develoment Database Configuration
-  const config = {
+  config = {
     host: "localhost",
     port: 3306,
     user: "root",
